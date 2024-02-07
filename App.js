6c1d08faa7f50ee1351a824e62694ec99a95d6fd
Nur Alibasyah <nuralibasyah@gmail.com>
2024-02-07 13:50:06 +0700

    menambahkan db.js

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const { connectToDatabase } = require('./db'); // Assuming db.js is in the same directory
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Routes
-app.get('/saldo', (req, res) => {
-    res.send("100000");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Connect to MongoDB before starting the server
+connectToDatabase().then(() => {
+    // Start the server
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch(err => {
+    console.error('Failed to connect to the database:', err);
 });
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nuralibasyah:Ini_Passwordku@cluster0.ti7srot.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://nuralibasyah:PpeqWhO99KFKF7yE@cluster0.ti7srot.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
